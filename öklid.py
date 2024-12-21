c5d8a8edf72496c4917e8580e071d90ee70dfbab
gizemhicyilmaz <gizemhcy@gmail.com>
2024-12-21 18:28:37 +0300

    minimum öklid mesafesinin hesaplanması

---
+++ öklid.py
@@ -0,0 +1,16 @@
+points = [(0, 0), (3, 4), (6, 8), (2, 1)]
+
+
+def euclideanDistance(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+
+distances = []
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+        min_distance = min(distances)
+print("Minimum mesafe:", min_distance)
